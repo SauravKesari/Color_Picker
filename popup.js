6276355e12cce22fbd0b99c27ad01a9762562a60
Saurav <Saurav47247@gmail.com>
2022-07-31 09:01:27 -0700

    file added

---
+++ popup.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector('.changeColorBtn');
+const colorGrid = document.querySelector('.colorGrid');
+const colorValue = document.querySelector('.colorValue');
+
+btn.addEventListener('click',async()=>{
+    chrome.storage.sync.get('color', ({ color }) => {
+        console.log('color: ', color);
+    });
+
+    let[tab]=await chrome.tabs.query({active:true,currentWindow:true});
+    chrome.scripting.executeScript({
+        target:{tabId:tab.id},
+        function:pickColor,
+    },async(injectionResults)=>{
+        const [data]=injectionResults;
+        if(data.result){
+            const color=data.result.sRGBHex;
+            colorGrid.style.backgroundColor=color;
+            colorValue.innerText+=color;
+            try{
+                await navigator.clipboard.writeText(color);
+            }catch(err){
+                console.log(err);
+            }
+        }
+    })
+});
+
+async function pickColor(){
+    try{
+        //picker
+      const eyedropper=new EyeDropper();
+      return await eyedropper.open();
+      //console.log(selectedColor);
+
+    }catch(err){
+        console.error(err);
+    }
+}
